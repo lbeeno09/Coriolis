78d5f62a33d3c47e0a721bf8bd1d56bf29fcceab
lbeeno09 <33786611+lbeeno09@users.noreply.github.com>
2025-08-20 10:22:03 +0900

    add double features

--- include/coriolis/precision.h
+++ include/coriolis/precision.h
@@ -4,6 +4,10 @@
 
 namespace coriolis
 {
+
+#if 1
+
+#define SINGLE_PRECISION
 typedef float real;
 #define REAL_MAX FLT_MAX
 #define real_sqrt sqrtf
@@ -15,4 +19,21 @@ typedef float real;
 #define real_fmod fmodf
 #define real_epislon FLT_EPSILON
 #define R_PI 3.14159f
+
+#else
+
+#define DOUBLE_PRECISION
+typedef double real;
+#define REAL_MAX DBLE_MAX
+#define real_sqrt sqrt
+#define real_abs fabs
+#define real_sin sin
+#define real_cos cos
+#define real_exp exp
+#define real_pow pow
+#define real_fmod fmod
+#define real_epislon DBL_EPSILON
+#define R_PI 3.14159264358979
+
+#endif
 }
